78595eadc40e50ba5532fe07307964930445c40d
Amanda Cameron <amanda@darkdna.net>
2017-08-12 11:48:04 -0400

    Switch to using the non-raw endpoint.

--- repo_files.go
+++ repo_files.go
@@ -1,17 +1,37 @@
 package gogitlab
 
+import "encoding/json"
+
 const (
-	repo_url_file_raw = "/projects/:id/repository/files/:file_path/raw"
+	repo_url_file = "/projects/:id/repository/files/:file_path"
 )
 
-// RepoFileRaw gets the specified file at the specified ref.
-func (g *Gitlab) RepoFileRaw(id, ref, filepath string) ([]byte, error) {
+type RepoFile struct {
+	FileName     string `json:"file_name"`
+	FilePath     string `json:"file_path"`
+	Size         int    `json:"size"`
+	Encoding     string `json:"encoding"`
+	Content      []byte `json:"content"`
+	Ref          string `json:"ref"`
+	BlobID       string `json:"blob_id"`
+	CommitID     string `json:"commit_id"`
+	LastCommitID string `json:"last_commit_id"`
+}
+
+// RepoFile gets the specified file at the specified ref.
+func (g *Gitlab) RepoFile(id, ref, filepath string) (*RepoFile, error) {
 	url, opaque := g.ResourceUrlRaw(repo_url_file_raw, map[string]string{
 		":id":        id,
 		":file_path": filepath,
 	})
 	url += "&ref=" + ref
 
+	var fileInfo RepoFile
+
 	contents, err := g.buildAndExecRequestRaw("GET", url, opaque, nil)
-	return contents, err
+	if err == nil {
+		err = json.Unmarshal(contents, &fileInfo)
+	}
+
+	return &fileInfo, err
 }
