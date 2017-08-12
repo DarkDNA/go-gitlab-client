6d371329ea45514fce4c65cf165afb9e99029f77
Amanda Cameron <amanda@darkdna.net>
2017-08-12 11:21:03 -0400

    Add beginnings of repo files API

---
+++ repo_files.go
@@ -0,0 +1,17 @@
+package gogitlab
+
+const (
+	repo_url_file_raw = "/projects/:id/repository/files/:file_path/raw"
+)
+
+// RepoFileRaw gets the specified file at the specified ref.
+func (g *Gitlab) RepoFileRaw(id, ref, filepath string) ([]byte, error) {
+	url, opaque := g.ResourceUrlRaw(repo_url_file_raw, map[string]string{
+		":id":        id,
+		":file_path": file_path,
+	})
+	url += "&ref=" + ref
+
+	contents, err := g.buildAndExecRequestRaw("GET", url, opaque, nil)
+	return contents, err
+}
